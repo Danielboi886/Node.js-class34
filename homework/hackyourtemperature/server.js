402d08fea2e1f086dfdb9279f1a4d6730fa1f185
Daniel Wang <Daniel.y.wang95@gmail.com>
2021-12-14 22:55:37 +0100

    Homework week 1

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello from backend to frontend!");
+});
+
+app.listen(port, () => {
+  console.log(`Your app is listening to port ${port}`);
+});
