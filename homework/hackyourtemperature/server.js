e55e2a55443e8f3263a7d0333c5a90fdbe0919f7
Daniel Wang <Daniel.y.wang95@gmail.com>
2021-12-15 17:08:23 +0100

    POST request added

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import http from "http";
 const app = express();
 const port = 3000;
 
@@ -8,4 +9,9 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Your app is listening to port ${port}`);
+});
+app.use(express.json());
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
 });
